fd5bedda888ac7306e8bd2da828b6ded39180d19
AlexKolpa <akolpa1990@gmail.com>
2014-12-13 22:42:19 +0100

    Added Check so rm doesn't fail on empty dirs

--- build-data.sh
+++ build-data.sh
@@ -1,6 +1,6 @@
 [ -d matches ] || mkdir matches
 [ -f master-zones.csv ] || (echo "Could not find 'master-zones.csv' file!" && exit 1)
-rm matches/*
+[ "$(ls -A matches)" ] && rm matches/*
 echo "Extracting matches from 'master-zones.csv'"
 awk -F, 'NR>1{gsub(/"/,"",$11);print $2", "$3", "$5", "$6", "$7", "$9", "$12 > "matches/"$4"-"$11".csv" }' master-zones.csv
 npm install
